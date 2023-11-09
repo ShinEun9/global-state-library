9e6cc5ee5bf8e9d31e42481313965bac031b67ac
ShinEun9 <ses2201@naver.com>
2023-11-09 13:29:10 +0900

    Recoil로 dark mode 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useRecoilState } from "recoil";
+import darkModeState from "../store/darkmodeState";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const [isDark, setIsDark] = useRecoilState(darkModeState);
+
+  const toggleThema = () => {
+    setIsDark((isDark) => !isDark);
+  };
   return (
     <footer className={`footer ${isDark ? "dark" : "white"}`}>
       <button onClick={toggleThema}>테마 변경</button>
