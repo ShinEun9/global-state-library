68d343d363e1077305faf83ecc5c4b7412514a6d
ShinEun9 <ses2201@naver.com>
2023-11-09 13:45:52 +0900

    Zustand로 dark mode 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import useStore from "./store/useStore";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const isDark = useStore((state) => state.isDark);
+  const toggleThema = useStore((state) => state.toggleThema);
+
   return (
     <footer className={`footer ${isDark ? "dark" : "white"}`}>
       <button onClick={toggleThema}>테마 변경</button>
