52119af1a36cfd78248a646a645521e54875c0a6
ShinEun9 <ses2201@naver.com>
2023-11-09 14:52:15 +0900

    Jotai로 dark mode 구현

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,13 @@
 import React from "react";
+import { useAtom } from "jotai";
+import darkModeAtom from "../atoms/darkMode";
 
-export default function Footer({ isDark, toggleThema }) {
+export default function Footer() {
+  const [isDark, setIsDark] = useAtom(darkModeAtom);
+  const toggleThema = () => {
+    setIsDark((isDark) => !isDark);
+  };
+
   return (
     <footer className={`footer ${isDark ? "dark" : "white"}`}>
       <button onClick={toggleThema}>테마 변경</button>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useAtomValue } from "jotai";
+import darkModeAtom from "../atoms/darkMode";
 
-export default function Header({ isDark }) {
+export default function Header() {
+  const isDark = useAtomValue(darkModeAtom);
+
   return (
     <header className={`header ${isDark ? "dark" : "white"}`}>
       <h1>Welcome! 프론트엔드 테킷 수강생</h1>
